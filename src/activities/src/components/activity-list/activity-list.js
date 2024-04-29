6a7fe701365f20b02a57aea1e9b27e55366327e7
Ankit Kesari <Ankit.Kesari@precisely.com>
2024-04-29 14:03:31 +0530

    Change butto style

--- src/activities/src/components/activity-list/activity-list.js
+++ src/activities/src/components/activity-list/activity-list.js
@@ -6,7 +6,9 @@ export default function ActivityList() {
   let navigate = useNavigate();
   return (
     <div className="activities-container">
-      <Button onClick={() => navigate('new-activity')}>New Activity</Button>
+      <Button onClick={() => navigate('new-activity')} kind="secondary" size="md">
+        New Activity
+      </Button>
       <div className="activities-list-container">
         <p>Activity List table will come here</p>
       </div>
